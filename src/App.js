91838393304d9d3dc944892b9b26298f0fb87547
Kpeale Legbara <66691866+kpeale@users.noreply.github.com>
2023-02-22 19:56:36 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import login from './login';
+
+
+function App() {
+  return (
+   <section className="login__section">
+    <login login={login}></login>
+   </section>
+  );
+}
+
+export default App;
---
+++ src/login.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const login = () => {
+  <div className="login__section--container1">
+    <h1 className="heading__primary">CSS
+        <span className="heading__secondary">Bookshops</span>
+    </h1>
+  </div>
+}
+export default login;
